5f1d4692da05987f9835b04f7eda5d43e6b516d4
tiboun <bounkong.khamphousone@ebiznext.com>
2020-11-01 15:03:55 +0100

    feat(varsubst): substitute variables in a string
    Add the ability to substitute shell like variables
    from values provided by a BaseResolver.
    
    Currently there is two implementation of it :
    
     - EnvResolver
     - DictResolver

---
+++ src/varsubst/interpolator.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2020 Bounkong Khamphousone
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and
+# this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import re
+from typing import AnyStr, Pattern
+
+from varsubst.exceptions import KeyUnresolvedException
+from varsubst.resolvers import BaseResolver
+
+
+class BaseInterpolator:
+    """
+    Abstract base class for all interpolators. Don't instantiate it.
+
+    An interpolator is intented to render a template with values
+    provided by a resolver.
+    """
+
+    def render(self, template: str, resolver: BaseResolver,
+               fail_on_unresolved: bool) -> str:
+        """
+        :param template: String with shell-like variables.
+            Allowed variables are of the form:
+                - $MY_SIMPLE_VAR
+                - ${MY_SIMPLE_VAR}
+                - ${USER-default}
+                - ${USER-$DEFAULT_USER}
+                - ${USER:-default}
+                - ${USER-$DEFAULT_USER}
+        """
+        pass
+
+
+class ShellInterpolator(BaseInterpolator):
+
+    _simple_re: Pattern[AnyStr] = re.compile(r'(?<!\\)\$([A-Za-z0-9_]+)')
+    """
+    For a given string, will match ${anystring1:-anystring2} or
+    ${anystring1-anystring2}
+
+    Groups extracted are :
+        1. anystring1
+        2. :-anystring2 or -anystring2
+        3. :- or -
+        4. anystring2
+    """
+    _extended_re: Pattern[AnyStr] = re.compile(
+        r'(?<!\\)\$\{([A-Za-z0-9_]+)((:?-)([^}]+))?\}')
+
+    def _repl_simple_env_var(self, resolver: BaseResolver,
+                             fail_on_unresolved: bool) -> str:
+        """
+        :param resolver: an instance which return a value given a key
+        :param fail_on_unresolved: if true, will throw an exception.
+        """
+        def substitute(m):
+            var_name = m.group(1)
+            result = resolver.resolve(var_name)
+            if result is None and fail_on_unresolved:
+                raise KeyUnresolvedException(var_name)
+            return result or ''
+
+        return substitute
+
+    def _repl_extended_env_var(self, resolver: BaseResolver,
+                               fail_on_unresolved: bool) -> str:
+        """
+        :param resolver: an instance which return a value given a key
+        :param fail_on_unresolved: if true, will throw an exception.
+        """
+        def substitute(m):
+            var_name = m.group(1)
+            result = resolver.resolve(var_name)
+            default_spec = m.group(2)
+            if default_spec:
+                default_raw = m.group(4)
+                if m.group(3) == ':-':
+                    # use default if var is unset or empty
+                    if not result:
+                        result = ShellInterpolator._simple_re.sub(
+                            self._repl_simple_env_var(resolver,
+                                                      fail_on_unresolved),
+                            default_raw)
+                elif m.group(3) == '-':
+                    # use default if var is unset
+                    if result is None:
+                        result = ShellInterpolator._simple_re.sub(
+                            self._repl_simple_env_var(resolver,
+                                                      fail_on_unresolved),
+                            default_raw)
+                else:
+                    raise RuntimeError('Unexpected string matched regex')
+            else:
+                if result is None and fail_on_unresolved:
+                    raise KeyUnresolvedException(var_name)
+            return result or ''
+
+        return substitute
+
+    def render(self, template: str, resolver: BaseResolver,
+               fail_on_unresolved: bool) -> str:
+        """
+        Substitute shell like variables in the given template
+        The following forms are supported:
+        Simple variables - will use an empty string if the variable is unset
+        $FOO
+        Bracketed expressions
+        ${FOO}
+            identical to $FOO
+        ${FOO:-somestring}
+            uses "somestring" if $FOO is unset, or set and empty
+        ${FOO-somestring}
+            uses "somestring" only if $FOO is unset
+        :param str string: A string possibly containing environment variables
+        :return: The string with env variable specs replaced with their values
+        """
+        # handle simple un-bracketed env vars like $FOO
+        first_pass = ShellInterpolator._simple_re.sub(
+            self._repl_simple_env_var(resolver, fail_on_unresolved),
+            template)
+
+        # handle bracketed env vars with optional default specification
+        return ShellInterpolator._extended_re.sub(
+            self._repl_extended_env_var(resolver, fail_on_unresolved),
+            first_pass)
---
+++ src/varsubst/resolvers.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# MIT License
+#
+# Copyright (c) 2020 Bounkong Khamphousone
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and
+# this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from os import environ
+from typing import Dict, Optional
+
+
+class BaseResolver:
+    """
+    Abstract base class for all resolvers. Don't instantiate it.
+
+    A resolver is intented to provide values from keys.
+    """
+
+    def resolve(self, key: str) -> Optional[str]:
+        """
+        Resolver should be able to produce a value for a given key.
+        If key doesn't exist, should return None.
+        """
+
+
+class EnvResolver(BaseResolver):
+    """
+    EnvResolver provide a value based on environnement variables.
+    """
+
+    def resolve(self, key: str) -> Optional[str]:
+        """
+        Resolver should be able to produce a value for a given key.
+        If key doesn't exist, should return None.
+        """
+        return environ.get(key)
+
+
+class DictResolver(BaseResolver):
+    """
+    DictResolver provide a value based on a given dictionary.
+    """
+
+    def __init__(self, dict: Dict[str, str]) -> None:
+        """
+        :param dict: dictionary used to resolve given keys
+        """
+        self.dict = dict
+
+    def resolve(self, key: str) -> Optional[str]:
+        """
+        Resolver should be able to produce a value for a given key.
+        If key doesn't exist, should return None.
+        """
+        return self.dict.get(key)
